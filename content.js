8fbaa768e8b5bd0c8b0c8613b38162c1023dd7a8
51eating <21963949+dnjstlr555@users.noreply.github.com>
2023-09-25 23:25:04 +0900

    Update content.js

--- content.js
+++ content.js
@@ -2,10 +2,14 @@ chrome.action.onClicked.addListener(function(tab) {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
         if (tabs.length > 0) {
           const currentTab = tabs[0];
-          const originalUrl = currentTab.url;
+          let originalUrl = currentTab.url;
           let proxy='https://libproxy.catholic.ac.kr/_Lib_Proxy_Url/'
           if(originalUrl.includes('libproxy.catholic.ac.kr')) {
             proxy='https://sci-hub.se/'
+            originalUrl = originalUrl.replace('.libproxy.catholic.ac.kr', '');
+            let splited=originalUrl.substr(originalUrl.indexOf("https://")+"https://".length).split("/")
+            splited[0]=splited[0].replaceAll("-",".")
+            originalUrl="https://"+splited.join("/")
           }
           const redirectUrl = proxy + originalUrl;
           chrome.tabs.create({ url: redirectUrl });
